0169e9f84ba4ea2119313e2f864a2b9a39c8e245
khk7759 <48235716+khk7759@users.noreply.github.com>
2020-09-25 23:38:09 +0900

    reactjs components, props, state

--- test01/src/App.js
+++ test01/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component { // 유사 자바스크립트. 자바스크립트인게 아님. JSX문법!
+  constructor(props){
+    // state값 초기화. 그 초기화 값을 가지고 컴포넌트 props 값을 세팅하려고 하는 것
+    super(props);
+
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Subject 
+        title={this.state.subject.title} 
+        sub={this.state.subject.sub}>
+        </Subject>
+        {/*<Subject title="WEB" sub="world wide web!"></Subject>*/}{/* props의 값들이 하드코딩 되어있음. 이 값을 state로 만들고 state 값을 subject라는 컴포넌트의 props로 전달하는 과정을 통해서 코드를 개선해보자! */}
+        {/*<Subject title="React" sub="For UI"></Subject>*/}
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
--- test01/src/index.js
+++ test01/src/index.js
@@ -1,15 +1,15 @@
+//entry 파일 (진입 파일)
+// ※  npm run build 빌드 명령어
+// 1) 개발할 때는 npm run start --> 이것저것 기능들이 많이 들어있어서 엄청 용량이 크다.
+// 2) 개발하다가 어플리케이션을 완성한 후에는 npm run build --> 용량을 엄청 줄여준다.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
